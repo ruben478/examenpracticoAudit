57286b4d7b5459687b567f120ffd7a3db4d529fb
rubenquispe <rubetevez1233@gmail.com>
2022-12-03 18:16:40 +0100

    rubenquispe

---
+++ app/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class User extends Authenticatable implements Auditable
+{
+    use Notifiable;
+    use \OwenIt\Auditing\Auditable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','level'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
